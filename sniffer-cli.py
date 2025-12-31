0639c8ce9e78d8c8190aab0ee64b6edaeb3e4dfa
beautiful <135546587+Greg4268@users.noreply.github.com>
2025-12-31 16:32:06 -0600

    Auto-commit on 2025-12-31 16:32:06 via git-auto

---
+++ sniffer-cli.py
@@ -0,0 +1,17 @@
+import argparse 
+from pathlib import Path 
+
+def sniff(): 
+    # do something 
+
+def get_arguments():
+    # do something 
+
+def main():
+    args = get_arguments() 
+
+    if args.save is True: 
+        # do something 
+
+if __name__ == "__main__":
+    main() 
